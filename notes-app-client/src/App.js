583c224039689f60551196e445f35d951bc67d2a
allen <theallenma@gmail.com>
2019-10-02 23:58:24 -0700

    add content to navbar and react router bootstrap

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import Routes from './Routes';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App container">
-      <Navbar collapseOnSelect>
+      <Navbar fluid collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
             <Link to="/">Scratch</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to='signup'>
+              <NavItem>Sign Up</NavItem>
+            </LinkContainer>
+            <LinkContainer to='login'>
+              <NavItem>Log In</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
