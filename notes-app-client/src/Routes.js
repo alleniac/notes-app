7cea6657b2043cd34a119d55ea754cf6877b40c5
allen <theallenma@gmail.com>
2019-10-14 15:25:28 -0700

    add signup form

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
+import Signup from './containers/Signup';
 import Login from './containers/Login';
 import NotFound from './containers/NotFound';
 
@@ -10,6 +11,7 @@ export default function Routes({ appProps }) {
         <Switch>
             <AppliedRoute path='/' exact component={ Home } appProps={ appProps }/>
             <AppliedRoute path='/login' exact component={ Login } appProps={ appProps }/>
+            <AppliedRoute path='/signup' exact component={ Signup } appProps={ appProps }/>
             { /* Finally, NotFound catches all unmatched routes */ }
             <Route component={ NotFound } />
         </Switch>
